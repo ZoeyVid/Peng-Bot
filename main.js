5c411fb59c88a8446297cc1e4dfd3c9c613abf3f
David <contact@davidcraft.de>
2022-09-25 17:53:53 +0200

    Update main.js
    Signed-off-by: David <contact@davidcraft.de>

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const client = new Client({
     GatewayIntentBits.GuildIntegrations,
     GatewayIntentBits.GuildMembers,
     GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
     GatewayIntentBits.DirectMessages
   ],
 });
